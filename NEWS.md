bb4c26c73bc457ce225228794a6b18a09d61e3a7
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-27 10:22:36 +0200

    Replace contents with pointer to new nanoc 4

--- NEWS.md
+++
@@ -1,5 +0,0 @@
-# nanoc-rdoc news
-
-## 1.0.0
-
-Initial release, transplanted from nanoc 3.6.3.
--- lib/nanoc/rdoc.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-module Nanoc
-  module RDoc
-  end
-end
-
-require 'nanoc/rdoc/version'
-require 'nanoc/rdoc/filter'
